f1cd437f21593fbee51c3187956cef141eec92fd
RockingWithTheBest <sikanweclyde@gmail.com>
2024-09-12 21:21:18 +0300

    first slide done

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -25,16 +25,68 @@
 // export default App;
 
 import React from 'react';
-// export default  function App() { 
-//   return (
-//     <h1>Function Component</h1>
-//   )
-// }
+import './styling.css';
+import searchImage from "./Photos/search.png";
+import livingRomm from "./Photos/livingroom.png"
 
-export default class App extends React.Component {
+const searchIcon = {
+  name: "Seacrh",
+  imageUrl: searchImage
+}
+const  LivingRoom= {
+  name : 'LivingRoom',
+  imageUrl: livingRomm
+}
+class FirstDescription extends React.Component {
   render() {
     return (
-      <h1>{this.props.type} Component</h1>
+      <div className = "detailsW31">
+        <span className = "dreamplace">Find your dream place</span>               
+        <span className="familyinminutes">Find house for your family in minutes</span>
+        <p className="lorem">
+            <span className ="loremP1">Find your place with an immersive photo experience and the most </span>
+            <span className="loremP2">listings, including things you won’t find anywhere else.</span> 
+        </p>
+      </div>
     )
   }
 }
+class LivingRoomPhoto extends React.Component{
+  render() {
+    return (
+      <div className="livingroomphoto">
+        <img src = {LivingRoom.imageUrl} />
+    </div>
+    )
+  }
+}
+class FirstSearchPhoto extends React.Component {
+  render() {
+    return (
+      <div className="detailsW32">
+              <div className="searchbar">
+                <img src = {searchIcon.imageUrl}/>
+                <span>Search</span>
+              </div>
+              <button className ="findproperty">Find property</button>
+       </div>
+    )
+  }
+}
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="WrapperOneThird">
+          <div className = "detail">
+              <h1>{this.props.type} Component</h1>
+              <FirstDescription />
+              <FirstSearchPhoto />
+              < LivingRoomPhoto/>
+          </div>  
+      </div>
+      
+      
+    )
+  }
+}
+
